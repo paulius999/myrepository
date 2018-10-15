9afc34456ec539b38875b7ca426427731bd0e10b
paulius999 <44171462+paulius999@users.noreply.github.com>
2018-10-15 20:13:40 +0300

    Update helokitty/src/helokitty/helloworld.java

--- helokitty/src/helokitty/helloworld.java
+++ helokitty/src/helokitty/helloworld.java
@@ -4,6 +4,14 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		float a = 1;
+		float b = 3;
+		float c = a/b;
+		if (c < 0) {
+		System.out.print("NIIIICE");
+		} else {
+			System.out.println("FU");
+		}
 	}
 
 }
