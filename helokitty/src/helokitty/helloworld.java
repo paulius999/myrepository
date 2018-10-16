5f92ef0f7e7559dad30512d90c0de1e2195696b5
paulius999 <44171462+paulius999@users.noreply.github.com>
2018-10-16 14:10:26 +0300

    aaa

--- helokitty/src/helokitty/helloworld.java
+++ helokitty/src/helokitty/helloworld.java
@@ -3,7 +3,16 @@ package helokitty;
 public class helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		for(int i=0; i<8;i++) {
+		System.out.println("helll");
+		float a = 1;
+		float b = 3;
+		float c = a/b;
+		if (c < 0) {
+			System.out.print("NIIIICE");
+		} else {
+			System.out.println("FU");
+		}
+		}
 	}
-
 }
